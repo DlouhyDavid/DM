a058a4493bc8ab6cc415c70a1a8a5a26e9a40bca
DlouhyDavid <david.dlouhy@tul.cz>
2023-04-27 11:54:30 +0000

    other image

--- tools/docker/startup.sh
+++ tools/docker/startup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# instalace balíčku shiny
-Rscript -e "install.packages('shiny', repos='http://cran.rstudio.com/')"
-
 # spuštění aplikace R Shiny
-R -e 'shiny::runApp("/srv/dm/web.R", port = 3838, host = "0.0.0.0")'
+R -e 'shiny::runApp("/srv/production/dm/R/web.R", port = 3838, host = "0.0.0.0")'
