bf3fae4c7d3c6d5cbc0152304076a65c55f1a316
DlouhyDavid <david.dlouhy@tul.cz>
2023-04-27 10:57:40 +0000

    test

---
+++ www/web.R
@@ -0,0 +1,23 @@
+library(shiny)
+
+# UI rozhraní
+ui <- fluidPage(
+  numericInput("num1", "První číslo:", value = 0),
+  numericInput("num2", "Druhé číslo:", value = 0),
+  actionButton("sum", "Sečíst"),
+  textOutput("result")
+)
+
+# serverová funkce
+server <- function(input, output) {
+  # reakce na tlačítko
+  observeEvent(input$sum, {
+    result <- input$num1 + input$num2
+    output$result <- renderText(paste("Výsledek je:", result))
+  })
+  
+}
+
+# spuštění aplikace
+app_instance <- shinyApp(ui, server)
+port <- runApp(app_instance, port = 8080)
