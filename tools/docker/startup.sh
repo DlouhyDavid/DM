b91a120b6613b6e6ac8af23cb664ce8b7912268c
DlouhyDavid <david.dlouhy@tul.cz>
2023-04-27 11:58:57 +0000

    fix path

--- tools/docker/startup.sh
+++ tools/docker/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # spuštění aplikace R Shiny
-R -e 'shiny::runApp("/srv/production/dm/R/web.R", port = 3838, host = "0.0.0.0")'
+R -e 'shiny::runApp("/srv/dm/R/web.R", port = 3838, host = "0.0.0.0")'
