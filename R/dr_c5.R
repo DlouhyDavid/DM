b60591d6bff80b095abcdd136e2f5a2f4a03cf7e
DlouhyDavid <david.dlouhy@tul.cz>
2023-05-01 23:24:21 +0000

    better presentation

--- R/dr_c5.R
+++ R/dr_c5.R
@@ -1,44 +1,30 @@
-#install.packages("C50")
+source("/workspaces/DM/R/utils.R")
+
 library(C50)
 library(stringr)
 
-test_data <- read.csv("/workspaces/DM/data/DRUG1n_test")
-test_data_factor <- data.frame(unclass(test_data), stringsAsFactors = TRUE) # Převední na factor
-test_data_factor$Sex <- as.factor(test_data_factor$Sex)
-test_data_factor$BP <- as.factor(test_data_factor$BP)
-test_data_factor$Cholesterol <- as.factor(test_data_factor$Cholesterol)
+training_data_file <- "/workspaces/DM/data/DRUG1n"
+testing_data_file <- "/workspaces/DM/data/DRUG1n_test"
 
-t_data <- read.csv("/workspaces/DM/data/DRUG1n")
-t_data$Na_to_K <- t_data$Na / t_data$K
-t_data$Na <- NULL
-t_data$K <- NULL
+testing_data <- load_data(testing_data_file)
+testing_data_factor <- get_factor(testing_data)
 
-t_data_factor <- data.frame(unclass(t_data), stringsAsFactors = TRUE) # Převední na factor
-t_data_factor$Sex <- as.factor(t_data_factor$Sex)
-t_data_factor$BP <- as.factor(t_data_factor$BP)
-t_data_factor$Cholesterol <- as.factor(t_data_factor$Cholesterol)
+training_data <- load_data(training_data_file)
+training_data <- prepare_data(training_data)
+training_data_factor <- get_factor(training_data)
 
-model_c50 <- C5.0(t_data_factor[, -5], t_data_factor$Drug)
-predikce <- predict(model_c50, test_data_factor)
+model_c50 <- C5.0(training_data_factor[, -5], training_data_factor$Drug)
+predikce <- predict(model_c50, testing_data_factor)
 
-x1 <- as.character(predikce)
-x2 <- test_data$Drug
-x3 <- str_detect(x1, x2, negate = TRUE)
-x <- data.frame(x1,x2, x3)
-pocet_rozdilu <- sum(x3)
-#
-View(x)
+score_table <- get_score_table(predikce, testing_data)
+score <- get_score(score_table)
 
-# Od uživatele
-new_data_factor <- data.frame(Age = as.integer(25), Sex = "M", BP = "HIGH", Cholesterol = "HIGH", Na_to_K = 1.247, stringsAsFactors = TRUE)
-new_data_factor$Sex <- as.factor(new_data_factor$Sex)
-new_data_factor$BP <- as.factor(new_data_factor$BP)
-new_data_factor$Cholesterol <- as.factor(new_data_factor$Cholesterol)
-new_data_factor$Sex <- factor(new_data_factor$Sex, levels=levels(t_data_factor$Sex))
-new_data_factor$BP <- factor(new_data_factor$BP, levels=levels(t_data_factor$BP))
-new_data_factor$Cholesterol <- factor(new_data_factor$Cholesterol, levels=levels(t_data_factor$Cholesterol))
+user_data_factor <- data.frame(
+    Age = as.integer(25),
+    Sex = "M", BP = "HIGH",
+    Cholesterol = "HIGH",
+    Na_to_K = 1.247,
+    stringsAsFactors = TRUE
+)
 
-predikce_uzivatel <- as.character(predict(model_c50, newdata = new_data_factor))
-
-View(predikce_uzivatel)
-View(t_data_factor)
+predikce_uzivatel <- recomend(user_data_factor, model_c50, training_data_factor)
