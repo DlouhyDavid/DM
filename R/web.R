22eb9b125e9fd198b0f9f23680c6f51645979055
DlouhyDavid <david.dlouhy@tul.cz>
2023-05-01 23:04:27 +0000

    demo models

--- R/web.R
+++ R/web.R
@@ -1,5 +1,26 @@
+source("/workspaces/DM/R/utils.R")
+
 library(shiny)
 library(shinythemes)
+library(C50)
+library(CHAID)
+library(stringr)
+
+training_data_file <- "/workspaces/DM/data/DRUG1n"
+testing_data_file <- "/workspaces/DM/data/DRUG1n_test"
+
+testing_data <- load_data(testing_data_file)
+testing_data_factor <- get_factor(testing_data)
+
+training_data <- load_data(training_data_file)
+training_data <- prepare_data(training_data)
+training_data_factor <- get_factor(training_data)
+
+model_chaid <- ctree(
+    Drug ~ Age + Sex + BP + Cholesterol + Na_to_K,
+    data = training_data_factor
+)
+model_c50 <- C5.0(training_data_factor[, -5], training_data_factor$Drug)
 
 # UI rozhraní s použitím tema "Flatly" z balíčku shinythemes
 ui <- fluidPage(
@@ -88,8 +109,25 @@ ui <- fluidPage(
 
 ## serverová funkce
 server <- function(input, output) {
-  observeEvent(input$submit, {
-    result <- "OK"
+    observeEvent(input$submit, {
+      user_data_factor <- data.frame(
+      Age = as.integer(25),
+      Sex = "M", BP = "HIGH",
+      Cholesterol = "HIGH",
+      Na_to_K = 1.247,
+      stringsAsFactors = TRUE
+    )
+    predikce_uzivatel_chaid <- recomend(
+      user_data_factor,
+      model_chaid,
+      training_data_factor
+    )
+    predikce_uzivatel_c50 <- recomend(
+      user_data_factor,
+      model_c50,
+      training_data_factor
+    )
+    result <- c("CHAID: ",predikce_uzivatel_chaid," | C5.0: ",predikce_uzivatel_c50)
     output$result <- renderText(paste(result))
   })
 }
