6e8c9ab6ede9b9338f700bbfe3c259d67fc70682
DlouhyDavid <david.dlouhy@tul.cz>
2023-05-02 19:56:03 +0000

    fix missing plot

--- R/init.R
+++ R/init.R
@@ -1,3 +1,3 @@
 #Instalace balíčků
-install.packages(c("shinythemes", "stringr", "C50"))
+install.packages(c("shinythemes", "stringr", "C50", "ggplot2"))
 install.packages("CHAID", repos = "http://R-Forge.R-project.org")
